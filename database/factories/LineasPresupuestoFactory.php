c669ce5a03821c7df760752c55bffd39cd6cbd61
RafaGC1911 <rafaelguijarro7@gmail.com>
2026-02-16 16:48:36 +0100

    Añadido foreign key a las factory de lineas y presupuesto

--- database/factories/LineasPresupuestoFactory.php
+++ database/factories/LineasPresupuestoFactory.php
@@ -17,9 +17,18 @@ class LineasPresupuestoFactory extends Factory
     public function definition(): array
     {
         return [
-            'precio' =>fake()->randomFloat(2,200,3000),
-            'cantidad' =>fake()->numberBetween(1,100),
-            //TODO INCLUIR PRESUPUESTO_ID Y PRODUCTO_ID
+            'precio' => fake()->randomFloat(2, 200, 3000),
+            'cantidad' => fake()->numberBetween(1, 100),
+            /**Para crear un presupuesto_id o un producto_id laravel primero llama a 
+             * la factory de presupuesto y a la de producto, porque las líneas no pueden existir 
+             * sin que haya antes un producto o un presupuesto, por eso laravel se asegura y si no existe, lo crea 
+             * para no generar conflicto
+             * Si no hay producto o presupuesto, laravel lo crea y le asigna su id.
+             * Esto se hace con las factories porque son datos falsos y de prueba.
+             */
+            'presupuesto_id' => \App\Models\Presupuesto::factory(),
+            'producto_id' => \App\Models\Producto::factory(),
+
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
+    //Crea un usuario
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@proyecto.com',
+            //Por defecto crea admin y contraseña admin
+            'rol' => 'admin',
+            'password' => Hash::make('admin')
         ]);
+
+        //Ahora llama a los seeders creados:
+        
+        
     }
 }
