837b3d5e15f5a6c7fb0876f6f0184c5226cfc348
RafaGC1911 <rafaelguijarro7@gmail.com>
2026-02-17 21:19:57 +0100

    Crear middleware para roles

--- app/Http/Middleware/CheckRoles.php
+++ app/Http/Middleware/CheckRoles.php
@@ -8,13 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CheckRoles
 {
-    /**
+    /**git
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, string $rol): Response
     {
+
         return $next($request);
     }
 }
