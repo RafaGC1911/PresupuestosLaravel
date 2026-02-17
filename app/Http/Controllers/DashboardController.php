f8a100d3911f943c9cf227c7c519a1e84c11f346
RafaGC1911 <rafaelguijarro7@gmail.com>
2026-02-17 21:45:49 +0100

    Terminada funcionalidad DashBoardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,17 +11,23 @@ use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request): View|RedirectResponse{
-$user = $request->user();
+    public function index(Request $request): View|RedirectResponse
+    {
+        $user = $request->user();
 
-if($user->isAdmin()){
-    return view('admin.dashboard');
-}
+        if ($user->isAdmin()) {
+            return view('admin.dashboard');
+        }
 
-if($user->isComercial()){
-    return view('comercial.dashboard');
-}
+        if ($user->isComercial()) {
+            return view('comercial.dashboard');
+        }
+
+
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        
+        return redirect()->route('login')->with('error', 'Rol de usuario no válido');
     }
-    
-
 }
