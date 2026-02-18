3f43af3b54f84d9da01457ec2f2a2228de232bbd
RafaGC1911 <rafaelguijarro7@gmail.com>
2026-02-18 20:29:48 +0100

    Terminado el seeder de presupuesto

--- database/seeders/PresupuestoSeeder.php
+++ database/seeders/PresupuestoSeeder.php
@@ -24,30 +24,66 @@ class PresupuestoSeeder extends Seeder
          */
         $productos = Producto::all();
         $clientes = Cliente::all();
-        $usuarios = User::where('rol', 'comercial')->get();
+        $usuarios = User::where('rol', User::ROL_COMERCIAL)->get();
 
         $clientes->each(function ($cliente) use ($productos, $usuarios){
+            // Creo un presupuesto 
             $presupuesto = Presupuesto::create([
+                //Relaciona el presupuesto con el cliente actual del bucle
                 'cliente_id' => $cliente->id,
+                //Elige un usuario comercial aleatorio
                 'user_id' => $usuarios->random()->id,
                 'fecha' => fake()->date(),
-                'total' => fake()->randomFloat(2, 300, 8000),
+                //El total lo incio en 0 porque se va a calcular luego
+                'total' => 0,
+                //Asigna un estado aleatorio del presupuesto entre estas 3 opciones
                 'estado' =>fake()->randomElement([
                 'pendiente',
                 'aceptado',
                 'rechazado'
             ])
             ]);
+            //Variable para ir almacenando el total real
+            $total = 0;
+
+            //Ahora hay que crear las líneas del presupuesto, le pongo un aleatorio entre 1 y 5 por ejemplo
+            $lineas = fake()->numberBetween(1,5);
+
+            for($i = 0; $i< $lineas; $i++){
+
+            //Saco un producto aleatorio 
+            $producto = $productos->random();
+
+            //Genero una cantidad aleatoria también
+             $cantidad = fake()->numberBetween(1, 10);
+
+             //Me traigo el precio base del producto
+             $precio = $producto->precio_base;
+
+             //Con esto calculo el subtotal de esta línea
+             $subtotal = $precio * $cantidad;
+
+             //Creo la línea del presupuesto. Laravel la va a relacionar automáticamente con el presupuesto
+             // gracias a la relación del modelo. Laravel le crea al presupuesto una línea que le pertenezca
+             $presupuesto->lineasPresupuestos()->create([
+
+             //Producto asociadoa la línea
+             'producto_id' => $producto->id,
+
+             //Precio del producto
+             'precio' => $precio,
+
+             //Cantidad
+             'cantidad' =>$cantidad
+             ]);
+
+             //Ahora suma el subtotal al total acumulado
+             $total += $subtotal;
+            }
+
+            //Por último se actualiza el total del presupuesto
+            $presupuesto->update(['total' =>$total]);
         });
 
-    
-
-        //  $table->id();
-        //     $table->foreignId('cliente_id')->constrained('clientes')->cascadeOnDelete();
-        //     $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-        //     $table->date('fecha');
-        //     $table->float('total');
-        //     $table->string('estado');
-        //     $table->timestamps();
     }
 }
