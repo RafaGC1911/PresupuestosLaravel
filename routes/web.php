6bed8a59f8214a656a0e845eba0b810b00c109f4
RafaGC1911 <rafaelguijarro7@gmail.com>
2026-02-25 18:08:40 +0100

    Creado ProductoController dentro del directorio Admin. Añadidas las rutas para este controlador en web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,17 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
+
+use App\Http\Controllers\Admin\ProductoController as AdminProductoController;
+/**
+ * significa: "ve a App/Http/Controllers/Admin/ProductoController.php —
+ *  que es el archivo que se ha generado con el comando artisan para el controlador de producto y 
+ * llámalo AdminProductoController para usarlo en este archivo".
+ */
+
+
+
 Route::get('/', function () {
     //Apuntar directamente a auth.login para que al iniciar la app no se vaya a la página de publicidad de laravel
     return view('auth.login');
@@ -22,5 +33,16 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Rutas del panel de administración
+// Solo accesibles si estás logueado, verificado y tienes rol admin
+Route::middleware(['auth', 'verified', 'rol:admin'])
+    ->prefix('admin')        // prefix es un método para que en la url salga admin
+    ->name('admin.')         // con ->name los nombres de ruta empezarán por admin
+    ->group(function () {
+        Route::resource('productos', AdminProductoController::class);
+    });
+
+
 
 require __DIR__.'/auth.php';
