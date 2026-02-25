3877e3b8d71a9416a577f57ef0e0b730f727c627
RafaGC1911 <rafaelguijarro7@gmail.com>
2026-02-25 17:22:36 +0100

    Resolver conflicto recuperando DashboardController

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+
+
+class DashboardController extends Controller
+{
+    public function index(Request $request): View|RedirectResponse
+    {
+        $user = $request->user();
+
+        if ($user->isAdmin()) {
+            return view('admin.dashboard');//TODO Cambiar por llamada al controlador
+        }
+
+        if ($user->isComercial()) {
+            return view('comercial.dashboard');//TODO Cambiar por llamada al controlador
+        }
+
+
+        //Si no tiene rol válido, cierra la sesión y redirige al login
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        
+        return redirect()->route('login')->with('error', 'Rol de usuario no válido');
+    }
+}
